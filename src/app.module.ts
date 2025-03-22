1db4986b171ad41b80639b3b486163976d8a2d4f
Ruben Merson Nzaou <rm.nzaou.etudes@gmail.com>
2025-03-21 22:59:38 -0400

    feat: added typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,33 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { validate } from './config/env.validation';
+import { EnvironmentVariables, validate } from './config/env.validation';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validate,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService<EnvironmentVariables, true>) => ({
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        database: configService.get<string>('DB_NAME'),
+        logging: configService.get<string>('DB_LOGGING') === 'true',
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        synchronize: configService.get('DB_SYNC') === 'true',
+        type: 'postgres',
+        cache: true,
+        autoLoadEntities: true,
+        entities: ['*.entity.ts'],
+      }),
     }),
   ],
   controllers: [AppController],
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-
-import { EnvironmentVariables } from './config/env.validation';
 
 @Injectable()
 export class AppService {
-  constructor(private readonly configService: ConfigService<EnvironmentVariables, true>) {}
+  constructor() {}
   getHello(): string {
     return 'Hello World!';
   }
