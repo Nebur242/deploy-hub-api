b228144a6ac01e689a0bb2ab88fbf8c3baeb1bb0
Ruben Merson Nzaou <rm.nzaou.etudes@gmail.com>
2025-03-21 22:43:41 -0400

    feat: added config service and env variables

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import { EnvironmentVariables } from './config/env.validation';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService<EnvironmentVariables, true>) {}
   getHello(): string {
     return 'Hello World!';
   }
