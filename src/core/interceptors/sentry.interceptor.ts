e09746e1f2689db087fb9f42f38d5fa10c32569e
Ruben From Mac Mini <rm.nzaou.etudes@gmail.com>
2025-03-23 23:49:32 -0400

    fix: copilot reviews

--- src/core/interceptors/sentry.interceptor.ts
+++ src/core/interceptors/sentry.interceptor.ts
@@ -5,13 +5,13 @@ import { tap } from 'rxjs/operators';
 
 @Injectable()
 export class SentryInterceptor implements NestInterceptor {
-  constructor(private readonly NODE_ENV: string) {}
+  constructor(private readonly nodeEnv: string) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<unknown> {
     return next.handle().pipe(
       tap({
         error: exception => {
-          if (this.NODE_ENV === 'local') {
+          if (this.nodeEnv === 'local') {
             return console.error('Sentry error:', exception);
           }
           Sentry.captureException(exception);
