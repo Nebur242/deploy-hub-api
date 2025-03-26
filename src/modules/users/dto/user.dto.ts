177c77d8b5988b9d85ec381702dfa362441fc1af
Ruben From Mac Mini <rm.nzaou.etudes@gmail.com>
2025-03-26 14:06:55 -0400

    fix: user dto duplicate

--- src/modules/users/dto/user.dto.ts
+++ src/modules/users/dto/user.dto.ts
@@ -21,9 +21,6 @@ export class CreateUserDto {
   @IsEnum(Role, { each: true })
   roles: `${Role}`[];
 
-  @IsEnum(Role, { each: true })
-  roles: `${Role}`[];
-
   @IsOptional()
   @IsString()
   company?: string;
